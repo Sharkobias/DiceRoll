4a83554dbe58412f9d9d6f0c283d35ac3b8d235b
TF <toby.fajks@gmail.com>
2024-02-11 16:40:36 +0100

    implemented interactive character creation

--- Dicerolltest.cpp
+++ Dicerolltest.cpp
@@ -3,36 +3,30 @@
 #include <map>
 using namespace std;
 
-int random;
+int _random;
 int rolls;
 string user_choice;
-/*
-//map<string,int >type=
-//{
-    {"D4",4},
-    {"D6",6},
-    {"D8",8},
-    {"D10",10},
-    {"D12",12},
-    {"D20",20}
-};
-*/
+
 enum GENDER {woman, man, intersex};
 enum RACE { human,elf,nymph};
+struct attributes 
+{
+    int strength;
+    int dexterity;
+    int constitution;
+    int intelligence;
+    int wisdom;
+    int charisma;
+};
 class character 
 {
 public:
-    character(GENDER gender, RACE race, string name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) 
+    character(GENDER gender, RACE race, string name, attributes attr)
     {
         _gender = gender;
         _race = race;
         _name = name;
-        _strength = strength;
-        _dexterity = dexterity;
-        _constitution = constitution;
-        _intelligence = intelligence;
-        _wisdom = wisdom;
-        _charisma = charisma;
+        _attr = attr;
     }
     GENDER _gender;
     string GetGender() 
@@ -48,7 +42,7 @@ public:
             case intersex:
                 return"intersex";
             default:
-                return"none";
+                return"other";
         }
     }
 
@@ -66,12 +60,12 @@ public:
         }
     }
     string _name;
-    int _strength;
-    int _dexterity;
-    int _constitution;
-    int _intelligence;
-    int _wisdom;
-    int _charisma;
+    attributes _attr;
+    int GetRandom() 
+    {
+        srand((unsigned)time(NULL));
+        _random = 1 + (rand() % 20);
+    }
 };
 
 void print_character(character new_character) 
@@ -79,26 +73,46 @@ void print_character(character new_character)
     cout << "my gender is " << new_character.GetGender() << endl;
     cout << "I am a" << " " << new_character.GetRace() << endl;
     cout << "my name is" << " " <<new_character._name << endl;
-    cout << "my strength is" << " " << new_character._strength << endl;
-    cout << "my dexternity is" << " " << new_character._dexterity << endl;
-    cout << "my constitution is" << " " << new_character._constitution << endl;
-    cout << "my intelligence is" << " " << new_character._intelligence << endl;
-    cout << "my wisdom is" << " " << new_character._wisdom << endl;
-    cout << "my charisma is" << " " << new_character._charisma << endl;
+    cout << "my strength is" << " " << new_character._attr.strength << endl;
+    cout << "my dexternity is" << " " << new_character._attr.dexterity << endl;
+    cout << "my constitution is" << " " << new_character._attr.constitution << endl;
+    cout << "my intelligence is" << " " << new_character._attr.intelligence << endl;
+    cout << "my wisdom is" << " " << new_character._attr.wisdom << endl;
+    cout << "my charisma is" << " " << new_character._attr.charisma << endl;
+    
 }
 
 int main()
 {
-    character Silverhand(man, human, "Johnny Silverhand", 13, 10, 16, 20, 18, 20);
+    /*character Silverhand(man, human, "Johnny Silverhand");
     print_character(Silverhand);
-    character Katrine(woman, elf, "Katrine", 10, 20, 14, 16, 13, 10);
-    print_character(Katrine);
+    character Katrine(woman, elf, "Katrine", {10, 20, 14, 16, 13, 10});
+    print_character(Katrine);*/
+    int Gender_input;
+    int Race_input;
+    string Name_input;
+    attributes attr;
+    cout << "What is the gender of your character" << endl;
+    cout << "[0]woman" << " " << "[1]man" << " " << "[2]intersex" << " " << "[3]other" << endl;
+    cin >> Gender_input;
+    cout << "what race is your character" << endl;
+    cout << "[0]human" << " " << "[1]elf" << " " << "[2]nymph" << endl;
+    cin >> Race_input;
+    cout << "Name your character" << endl;
+    cin >> Name_input;
+    cout << "my strength is" <<endl;
+    cin >> attr.strength;
+    cout << "my dexternity is" <<endl;
+    cin >> attr.dexterity;
+    cout << "my constitution is" <<endl;
+    cin >> attr.constitution;
+    cout << "my intelligence is"<<endl;
+    cin >> attr.intelligence;
+    cout << "my wisdom is"  <<endl;
+    cin >> attr.wisdom;
+    cout << "my charisma is" <<endl;
+    cin >> attr.charisma;
 
-
-    do
-    {
-        srand((unsigned)time(NULL));
-        random = 1 + (rand() % 6);
-        return random;
-    } while (rolls > 0);
+    character player(static_cast<GENDER>(Gender_input), static_cast<RACE>(Race_input), Name_input, attr);
+    print_character(player);
 }
